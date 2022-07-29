162fe755cc68d1c60d0ded912878fe420b9c5f5b
Siddhesh Chaudhari <106294707+9604308197@users.noreply.github.com>
2022-07-29 14:32:47 +0530

    Add files via upload

---
+++ Shaurya/src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ReactformComponent } from './reactform/reactform.component';
+import { TableComponent } from './table/table.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ReactformComponent,
+    TableComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
